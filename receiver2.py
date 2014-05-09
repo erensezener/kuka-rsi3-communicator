0ee67b5659236219b084dfb215c00220442ceb4d
Eren Sezener <erensezener@gmail.com>
2014-05-09 12:24:21 +0300

    Make the server functional

--- receiver2.py
+++ receiver2.py
@@ -1,5 +1,4 @@
 import socket
-from BeautifulSoup import BeautifulSoup
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5006
--- server.py
+++ server.py
@@ -1,34 +1,36 @@
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 49152
+UDP_IP = "10.100.48.101" # IP Address of the External PC
+UDP_PORT = 49152 # Port of the External PC
 BUFFER_SIZE = 1024
+XML_FILE_NAME = "ExternalData.xml"
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create a UDP socket
+    sock.bind((UDP_IP, UDP_PORT))
 
+    while True:
+        xml_file = open(XML_FILE_NAME, "r")
 
-def update_timestamp(recieved_data, data_to_send):
-    ipoc_begin_index = recieved_data.index("<IPOC>")
-    ipoc_end_index = recieved_data.index("</IPOC>")
-    recieved_ipoc = recieved_data[ipoc_begin_index + 6: ipoc_end_index]
+        received_data, socket_of_krc = sock.recvfrom(BUFFER_SIZE) # buffer size is 1024 bytes
+        print received_data
+        data_to_send = xml_file.read()
+        data_to_send = mirror_timestamp(received_data, data_to_send)
+
+        sock.sendto(data_to_send, socket_of_krc)
+        xml_file.close()
+
+# Updates the timestamp of the data to send based on the timestamp of the received data
+def mirror_timestamp(received_data, data_to_send):
+    ipoc_begin_index = received_data.index("<IPOC>")
+    ipoc_end_index = received_data.index("</IPOC>")
+    received_ipoc = received_data[ipoc_begin_index + 6: ipoc_end_index]
 
     old_ipoc_begin_index = data_to_send.index("<IPOC>")
     old_ipoc_end_index = data_to_send.index("</IPOC>")
     old_ipoc = data_to_send[old_ipoc_begin_index + 6: old_ipoc_end_index]
 
-    return data_to_send.replace("<IPOC>"+old_ipoc+"</IPOC>", "<IPOC>"+recieved_ipoc+"</IPOC>")
+    return data_to_send.replace("<IPOC>"+old_ipoc+"</IPOC>", "<IPOC>"+received_ipoc+"</IPOC>")
 
-
-while True:
-    xmlFile = open("ExternalData.xml", "r")
-
-    recieved_data, socket_of_krc = sock.recvfrom(BUFFER_SIZE) # buffer size is 1024 bytes
-
-    data_to_send = xmlFile.read()
-    data_to_send = update_timestamp(recieved_data, data_to_send)
-
-    sock.sendto(data_to_send, socket_of_krc)
-
-    xmlFile.close()
+if __name__ == "__main__":
+    main()
