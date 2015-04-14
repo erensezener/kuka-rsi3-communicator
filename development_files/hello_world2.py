33dd6e25e3ecd62df22712699844b887898d2c5c
Eren Sezener <erensezener@gmail.com>
2015-04-14 09:43:44 -0400

    update hello world

--- development_files/hello_world2.py
+++ development_files/hello_world2.py
@@ -14,6 +14,7 @@ Known bugs: -
 
 import socket
 import time
+import math
 
 
 TCP_IP = '10.100.48.101'
@@ -23,18 +24,14 @@ BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
+w = 2 * math.pi * 0.1
+amp = 3 * math.pi / 180.0
+
 count = 0
-flag = -1
 while True:
-    if flag == 1:
-        count = count +1
-        if count > 50:
-            flag = -1
-    else:
-        count = count -1
-        if count < -50:
-            flag = 1
-    text = 'a,' + str(flag*0.15) + ',0,0,0,0,0'
+    count = count + 1
+    t = 0.004 * count
+    text = 'a,' + str(amp * w * math.cos(w * t)) + ',' + str(amp * w * math.cos(w * t)) + ',' + str(amp * w * math.cos(w * t)) + ',' + str(amp * w * math.cos(w * t)) + ',' + str(amp * w * math.cos(w * t)) + ',' + str(amp * w * math.cos(w * t)) 
     s.send(text)
     data = s.recv(BUFFER_SIZE)
     print "received data:", data
